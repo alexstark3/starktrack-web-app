349e99257059b3b552b9a7531fb94f206d99612b
alexstark3 <a.stojkov@gmail.com>
2025-07-16 15:45:38 +0200

    v521

--- lib/widgets/company/company_side_menu.dart
+++ lib/widgets/company/company_side_menu.dart
@@ -25,7 +25,7 @@ class CompanySideMenu extends StatelessWidget {
     required this.menuItems,
     this.compact = false,
     this.showBrand = true,
-    this.version = 'Stark Track v1.520b', // <-- Default is empty
+    this.version = 'Stark Track v1.521b', // <-- Default is empty
   });
 
   @override
